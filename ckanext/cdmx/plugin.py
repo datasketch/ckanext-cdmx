ab299e34e9766129c9b1a61fa605bf46c50551ef
David Daza <03dazal@gmail.com>
2022-03-18 23:55:11 -0500

    Add filter field to resource schema

---
+++ ckanext/cdmx/plugin.py
@@ -0,0 +1,41 @@
+from json import tool
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class CdmxPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+
+    def is_fallback(self):
+        return True
+
+    def package_types(self):
+        return []
+
+    def _modify_package_schema(self, schema):
+        schema['resources'].update({
+            'resource_filters': [toolkit.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        schema = super(CdmxPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(CdmxPlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(CdmxPlugin, self).show_package_schema()
+        schema['resources'].update({
+            'resource_filters': [toolkit.get_validator('ignore_missing')]
+        })
