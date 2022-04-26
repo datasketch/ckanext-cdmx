efaafe97a5432d0b552b12d0ff06bc4020455fb7
David Daza <03dazal@gmail.com>
2022-04-26 07:34:08 -0500

    Add vocabulary with date formats

--- ckanext/cdmx/plugin.py
+++ ckanext/cdmx/plugin.py
@@ -1,12 +1,50 @@
-from json import tool
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+
+
+# TODO: update vocabulary
+def create_date_formats():
+    user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+    try:
+        data = {'id': 'date_formats'}
+        print(toolkit.get_action('vocabulary_show')(context, data))
+        toolkit.get_action('vocabulary_show')(context, data)
+    except toolkit.ObjectNotFound:
+        data = {'name': 'date_formats'}
+        vocab = toolkit.get_action('vocabulary_create')(context, data)
+        # By definition, the name for the new tag must be a string between 2 and 100 characters long containing only alphanumeric characters and -, _ and . See https://docs.ckan.org/en/2.9/api/index.html?highlight=tag_create#ckan.logic.action.create.tag_create
+        # Update the readable value of each tag in readable_date_formats helper function
+        for tag in (u'd_m_a', u'a_m_d', u'd_m_a HH.MM', u'a_m_d HH.MM'):
+            data = {'name': tag, 'vocabulary_id': vocab['id']}
+            toolkit.get_action('tag_create')(context, data)
+
+
+def date_formats():
+    create_date_formats()
+    try:
+        tag_list = toolkit.get_action('tag_list')
+        date_formats = tag_list({}, {'vocabulary_id': 'date_formats'})
+        return date_formats
+    except toolkit.ObjectNotFound:
+        return None
+
+
+def readable_date_formats():
+    return {'d_m_a': 'd/m/a', 'a_m_d': 'a/m/d', 'd_m_a HH.MM': 'd/m/a HH:MM', 'a_m_d HH.MM': 'a/m/d HH:MM'}
 
 
 class CdmxPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IDatasetForm)
     plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.ITemplateHelpers)
+
+
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {'date_formats': date_formats, 'readable_date_formats': readable_date_formats}
 
     # IConfigurer
 
@@ -28,6 +66,12 @@ class CdmxPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
             'resource_filters': [toolkit.get_validator('ignore_missing')],
             'resourse_stat_text': [toolkit.get_validator('ignore_missing')]
         })
+        schema.update({
+            'date_format': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_converter('convert_to_tags')('date_formats')
+            ]
+        })
         return schema
 
     def create_package_schema(self):
@@ -45,5 +89,11 @@ class CdmxPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         schema['resources'].update({
             'resource_filters': [toolkit.get_validator('ignore_missing')],
             'resourse_stat_text': [toolkit.get_validator('ignore_missing')]
+        })
+        schema['tags']['__extras'].append(toolkit.get_converter('free_tags_only'))
+        schema.update({
+            'date_format': [
+                toolkit.get_converter('convert_from_tags')('date_formats'),
+                toolkit.get_validator('ignore_missing')]
         })
         return schema
