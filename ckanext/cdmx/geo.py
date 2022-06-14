dd11fc90483fcb2b5867ca0079e444d13c90f7b6
David Daza <03dazal@gmail.com>
2022-06-14 16:01:42 -0500

    Update resource read template

--- ckanext/cdmx/geo.py
+++ ckanext/cdmx/geo.py
@@ -1,26 +1,33 @@
+import logging
 import ckan.plugins.toolkit as toolkit
 import geopandas
 import os
 
-TEMPDIR = os.path.join(os.path.dirname(__file__), '..', 'tmp')
-OUTDIR = os.path.join(TEMPDIR, 'out')
+
+def log(msg, level=logging.INFO):
+    logger = logging.getLogger(u'ckan')
+    logger.log(level, msg)
+
 
 def shp2geojson(resource_id):
-    if not os.path.isdir(OUTDIR):
-        os.makedirs(OUTDIR)
-    
+    log('shp2geojson')
+    log(os.environ.get('USERNAME'))
     user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
     context = {'user': user['name']}
-    resource = toolkit.get_action('resource_show')(context, { 'id': resource_id })
+    resource = toolkit.get_action('resource_show')(
+        context, {'id': resource_id})
     res_name = resource['name']
-    outfile = os.path.join(OUTDIR, res_name)
+    package_id = resource['package_id']
+    outfile = '/var/tmp/{}'.format(res_name)
+    log('Write access to /var/tmp')
+    log(os.access('/var/tmp', os.W_OK))
     gdf = geopandas.read_file(resource['url'])
+    log(gdf)
     gdf.to_file(outfile, driver='GeoJSON')
     toolkit.get_action('resource_create')(context, {
-      'package_id': resource['package_id'],
-      'format': 'GeoJSON',
-      'name': res_name,
-      'url': 'any',
-      'upload': open(outfile)
+        'package_id': package_id,
+        'name': res_name,
+        'format': 'CSV',
+        'url': 'https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv',
     })
-    
+    log('.........')
