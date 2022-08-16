94ac721d725f282dce4a557baa572c5a724e0297
David Daza <03dazal@gmail.com>
2022-08-16 12:45:58 -0500

    Redirect on checkbox change

--- ckanext/cdmx/assets/javascript/modules/redirect.js
+++ ckanext/cdmx/assets/javascript/modules/redirect.js
@@ -7,10 +7,10 @@ ckan.module('redirect', function (jQuery) {
       this.el.on('change', jQuery.proxy(this._onChange, this))
     },
     _onChange: function(event) {
+      event.preventDefault()
       const origin = window.location.origin
       const href = this.options.href
-      event.preventDefault()
-      console.log(origin + href);
+      window.location = origin + href
     }
   }
 })
