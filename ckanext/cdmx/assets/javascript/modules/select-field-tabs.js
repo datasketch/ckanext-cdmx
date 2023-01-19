ebef725b8c65ee7ad35a83e2cac330825ee2529d
David Daza <03dazal@gmail.com>
2023-01-19 11:01:25 -0500

    Remove name attr of hidden inputs

--- ckanext/cdmx/assets/javascript/modules/select-field-tabs.js
+++ ckanext/cdmx/assets/javascript/modules/select-field-tabs.js
@@ -3,21 +3,45 @@ ckan.module('select-field-tabs', function (jQuery) {
     initialize () {
       this.panelsContainer = document.getElementById(this.options.panels)
       this.panels = Array.prototype.map.call(this.panelsContainer.children, el => el)
-      this._showPanel(this.panels, this.options.open)
+      this._extendAttrs.call(this)
+      this._showPanel.call(this)
       
       this.el.on('change', jQuery.proxy(this._onChange, this))
     },
     _onChange: function (event) {
-      this._showPanel(this.panels, event.target.value)
+      this._showPanel.call(this, event.target.value)
     },
-    _showPanel: function (panels, value) {
-      panels.forEach(function (panel) {
-        if (panel.id === value) {
-          panel.style.display = 'block'
+    _showPanel: function (value) {
+      const option = value || this.options.open
+      this.panels.forEach((panel) => {
+        if (panel.id === option) {
+          this._enable(panel)
         } else {
-          panel.style.display = 'none'
+          this._disable(panel)
         }
       })
+    },
+    _extendAttrs: function () {
+      this.panels.forEach(function (panel) {
+        const elements = panel.querySelectorAll('[name]')
+        elements.forEach(function (element) {
+          element.dataset.name = element.name
+        })
+      })
+    },
+    _enable: function (panel) {
+      panel.style.display = 'block'
+      const elements = panel.querySelectorAll('[data-name]')
+      elements.forEach(function (element) {
+        element.setAttribute('name', element.dataset.name)
+      })
+    },
+    _disable: function (panel) {
+      panel.style.display = 'none'
+      const elements = panel.querySelectorAll('[data-name]')
+      elements.forEach(function (element) {
+        element.removeAttribute('name')
+      })
     }
-  }
+   }
 })
