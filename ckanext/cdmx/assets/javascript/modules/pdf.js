1997dea1ee09e877e6373ac86d6d61978f196373
David Daza <03dazal@gmail.com>
2023-03-13 15:37:13 -0500

    Update pdf module

--- ckanext/cdmx/assets/javascript/modules/pdf.js
+++ ckanext/cdmx/assets/javascript/modules/pdf.js
@@ -2,7 +2,34 @@ ckan.module('pdf', function (jQuery) {
   return {
     initialize: function() {
       const doc = this.options.doc
-      console.log(doc);
+      // Loaded via <script> tag, create shortcut to access PDF.js exports.
+      const pdfjsLib = window['pdfjs-dist/build/pdf'];
+      console.log(pdfjsLib);
+      // The workerSrc property shall be specified.
+      // pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+      const loadingTask = pdfjsLib.getDocument(doc);
+      console.log(loadingTask);
+
+      loadingTask.promise.then(function (pdf) {
+        pdf.getPage(1).then(function (page) {
+          const scale = 1
+          const viewport = page.getViewport({ scale })
+
+          // Prepare canvas using PDF page dimensions
+          const canvas = document.getElementById(this.el.id);
+          const context = canvas.getContext('2d');
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+
+          const renderTask = page.render({
+            canvasContext: context,
+            viewport: viewport
+          });
+          renderTask.promise.then(function () {
+            console.log('Page rendered');
+          });
+        })
+      })
     }
   }
 })
