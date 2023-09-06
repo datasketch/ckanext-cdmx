2cec2921bc6e52afaa0b2d72e28bd2b4f5cf199e
pipeleon <oflg94@gmail.com>
2023-09-06 16:43:20 -0500

    changing url for iframe page visualizador

--- ckanext/cdmx/assets/javascript/modules/change-iframe.js
+++ ckanext/cdmx/assets/javascript/modules/change-iframe.js
@@ -3,13 +3,18 @@ ckan.module('change-iframe', function (jQuery) {
         initialize() {
             const root = this.el.get(0)
 
+            console.log(this.options.url);
+
             this.resources = root.querySelector('#resourceDropdown')
             this.frame = root.querySelector('#resourceFrame')
 
             this.resources.addEventListener('change', () => {
-               this.frame.src = `https://datasketch.shinyapps.io/cdmxApp/?ckanConf=${this.resources.value}` 
+                if (this.options.url == 'https://datos-prueba.cdmx.gob.mx'){
+                    this.frame.src = `https://services.datasketch.co/cdmxApp/?ckanConf=${this.resources.value}` 
+                } else {
+                    this.frame.src = `https://visdatos.cdmx.gob.mx/app_direct_i/cdmx/_/?ckanConf=${this.resources.value}`
+                }               
             })
-
         }
     }
 })
